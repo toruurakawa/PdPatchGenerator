54de9f4fef5327ffaea842760423ee4b2d9a7469
TORU URAKAWA <toru.urkw@gmail.com>
2013-05-02 16:31:20 +0900

    delete example from PdPatchCanvas.rb

--- PdPatchGenerator.rb
+++ PdPatchGenerator.rb
@@ -92,20 +92,3 @@ class PdConnection
 	end	
 
 end
-
-
-
-canvas = PdPatchCanvas.new(300, 300, "test")
-
-obj0 = PdObject.new(50, 50, "osc~")
-obj1 = PdObject.new(100, 100, "dac~")
-
-connection0 = PdConnection.new(obj0, 0, obj1, 0)
-connection1 = PdConnection.new(obj0, 0, obj1, 1)
-
-canvas.add_object(obj0)
-canvas.add_object(obj1)
-canvas.add_connection(connection0)
-canvas.add_connection(connection1)
-canvas.save()
-
